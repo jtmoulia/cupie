7b6f74090ff428bd991a437f58a29525dfb6fcce
Thomas Moulia <jtmoulia@gmail.com>
2017-08-18 22:42:36 -0700

    Add light script to wrap GPIO pin control

---
+++ cupie/light.py
@@ -0,0 +1,24 @@
+from RPi import GPIO
+import sys
+
+PIN = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN, GPIO.OUT)
+
+
+def on(pin=PIN):
+    GPIO.output(pin, 1)
+
+
+def off(pin=PIN):
+    GPIO.output(pin, 0)
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'on':
+        on()
+    elif sys.argv[1] == 'off':
+        off()
+    else:
+        raise ValueError('Unrecognized argument: {}'.format(sys.argv[1]))
