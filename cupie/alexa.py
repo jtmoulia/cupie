fb6ef5c464996e6ca82388ca58e6ec91f92f6196
Thomas Moulia <jtmoulia@gmail.com>
2017-08-20 23:04:28 -0700

    Work to get alexa working

--- cupie/alexa.py
+++ cupie/alexa.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from flask_ask import Ask, statement
 
+from cupie import light
+
+
 app = Flask(__name__)
 ask = Ask(app, '/alexa')
 
 
 @ask.intent('LightOnIntent')
 def light_on():
+    light.on()
     return statement("Light it up")
 
+@ask.intent('LightOffIntent')
+def light_off():
+    light.off()
+    return statement("Goodnight sweet prince")
 
 if __name__ == '__main__':
     app.run()
